bbf73297a036c3265e37a025b74fbc095059e466
IrynaMoroz <iryna.morozzz@gmail.com>
2022-04-07 20:15:09 +0300

    First commit

---
+++ bot.py
@@ -0,0 +1,63 @@
+import telebot
+import config
+import requests
+import time
+
+from bs4 import BeautifulSoup
+
+bot = telebot.TeleBot(config.TOKEN)
+
+BOT_URL = "https://git.heroku.com/telegrambothromadske.git"
+
+@bot.message_handler(content_types=['text'])
+def commands(message):
+	if message.text == "/start":
+		bot.send_message(message.chat.id, "Hello!\nNow you will get news from Hromadke.ua\nLast news:")
+
+		
+		URL = "https://hromadske.ua/news"
+
+		page = requests.get(URL)
+		soup =BeautifulSoup(page.content, "html.parser")
+
+		post = soup.find("a", class_="NewsPublicationCard")
+		#print(post)
+		bot.send_message(message.chat.id, post["data-vr-contentbox"]+"\n--------------------------------------------------"
+			+"\nRead full peace of news here "+ "https://hromadske.ua"+post["data-vr-contentbox-url"])
+
+
+
+		back_post_id = post.find("div", class_="NewsPublicationCard-time").text.strip()
+
+		#print("id is " + back_post_id)
+		while True:
+			post_text = parser(back_post_id)
+			back_post_id = post_text[1]
+
+			if post_text[0] != None:
+				bot.send_message(message.chat.id, (post_text[0]+"\n--------------------------------------------------"
+			+"\nRead full peace of news here "+ "https://hromadske.ua"+post_text[2]))
+				time.sleep(50)
+
+
+
+def parser(back_post_id):
+	URL = "https://hromadske.ua/news"
+
+	page = requests.get(URL)
+	soup =BeautifulSoup(page.content, "html.parser")
+
+	post = soup.find("a", class_="NewsPublicationCard")
+	post_id = post.find("div", class_="NewsPublicationCard-time").text.strip()
+	#print("id is " + post_id)
+	#url = soup.find("a", class_="href", id=True)#.text.strip()
+
+	text = post["data-vr-contentbox"]
+
+	if post_id != back_post_id:
+		return f"{text}", post_id, post["data-vr-contentbox-url"]
+	else:
+		return None, post_id
+
+# RUN
+bot.polling()#none_stop=True)
